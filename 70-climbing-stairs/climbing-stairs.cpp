85bed127525d0e25347e9a85047973d32993ab88
Gautamdas77 <127530101+Gautamdas77@users.noreply.github.com>
2025-12-26 03:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -7,9 +7,9 @@ public:
         for(int i=2;i<=n;i++){
             dp[i] = dp[i-1] + dp[i-2]; 
         }
+         return dp[n];
         //if(n==1 || n==0) return 1;
        // if(dp[n]!=-1) return dp[n];
-        return dp[n];
         //return solve(n,dp);
     }
 };
