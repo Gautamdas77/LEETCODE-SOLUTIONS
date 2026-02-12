f8ba704d94486d0ebbf23168554715c8028d683e
Gautamdas77 <127530101+Gautamdas77@users.noreply.github.com>
2026-02-12 16:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (83.05%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        int size = nums.size()/3;
-        vector<int>ans;
-        for(auto it:nums){
-            mpp[it]++;
-        }
-
-        for(auto it:mpp){
-            if(it.second>size){
-                ans.push_back(it.first);
+        int maj1 = NULL;
+        int maj2 = NULL;
+        int cnt1 = 0; 
+        int cnt2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==maj1){
+                cnt1++;
+            }
+            else if(nums[i]==maj2){
+                cnt2++;
+            }
+            else if(cnt1==0){
+                maj1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2==0){
+                maj2 = nums[i];
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        vector<int>ans;
+        int freq1 = 0;
+        int freq2 = 0;
+        for(auto it:nums){
+            if(it==maj1) freq1++;
+            else if(it==maj2) freq2++;
+        }
+        if(freq1>floor(nums.size()/3)) ans.push_back(maj1);
+        if(freq2>floor(nums.size()/3)) ans.push_back(maj2);
         return ans;
     }
 };
