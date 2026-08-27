4f3ed0a182f02eb7707cc8855a705c7694139378
Gautam Das <127530101+Gautamdas77@users.noreply.github.com>
2026-08-27 23:47:37 +0530

    Time: 3 ms (64.61%) | Memory: 8.5 MB (11.38%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,9 +3,10 @@ public:
     unordered_map<int,int>dp;
     int fib(int n) {
         if(n==0 || n==1) return n;
+
         if(dp.find(n)!=dp.end()) return dp[n];
         dp[n] = fib(n-1) + fib(n-2);
 
-        return fib(n-1) + fib(n-2);
+        return dp[n];
     }
 };
