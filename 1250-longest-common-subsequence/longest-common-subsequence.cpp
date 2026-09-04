f814bb608e5bf53ad046930f9c964de1aa1f3119
Gautam Das <127530101+Gautamdas77@users.noreply.github.com>
2026-09-04 13:53:37 +0530

    Time: 55 ms (5.23%) | Memory: 45.3 MB (6.53%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int fun(string&s1, string&s2, int n, int m , int i, int j, vector<vector<int>>&dp){
+        if(i==n || j==m) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j] = 1 + fun(s1,s2,n,m,i+1,j+1,dp);
+        int c1 = fun(s1,s2,n,m,i+1,j,dp);
+        int c2 = fun(s1,s2,n,m,i,j+1,dp);
+        return dp[i][j] = max(c1,c2);
+    }
+
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1);
+        for(int i=0;i<n;i++){
+            vector<int>t(m+1,-1);
+            dp[i] = t;
+        }
+        int i = 0;
+        int j = 0;
+
+        return fun(text1,text2,n,m,i,j,dp);
+    }
+};
