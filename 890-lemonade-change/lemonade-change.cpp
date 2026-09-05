79e36bc013751842a3cddce7e5699c8d44a99061
Gautam Das <127530101+Gautamdas77@users.noreply.github.com>
2026-09-05 12:24:17 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (44.69%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -4,9 +4,6 @@ public:
         int n = bills.size();
         int five = 0;
         int ten = 0;
-        int twenty = 0;
-        if(bills[0]!=5) return false;
-        if(bills[1]==20) return false;
         for(int i=0;i<n;i++){
             if(bills[i]==10){
                 if(five!=0){
@@ -24,7 +21,6 @@ public:
                     five = five -3;
                 }
                 else return false;
-                twenty++;
             }
             else five++;
         }
